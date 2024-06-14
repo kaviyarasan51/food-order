365d3978e0d19ff2e8ad3266272e518aecf5ac0a
[vengadaganapathy1] <upskilltechnology24@gmail.com>
2024-06-14 13:34:00 +0530

    Added scrollbar for Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,10 +6,26 @@ const imgData = ImagesData.data.cards[0].card.card.imageGridCards.info;
 
 export default Banner = () => {
   return (
-    <div className='img-container'>
-      {imgData.map((banner) => {
-        return <BannerImage key={banner.id} bannerContent={banner} />;
-      })}
+    <div>
+      <div className='btn-container'>
+        <button
+          onClick={() => {
+            document.getElementById("container").scrollLeft -= 160 * 4;
+          }}>
+          &lt;
+        </button>
+        <button
+          onClick={() => {
+            document.getElementById("container").scrollLeft += 160 * 4;
+          }}>
+          &gt;
+        </button>
+      </div>
+      <div className='img-container' id='container'>
+        {imgData.map((banner) => {
+          return <BannerImage key={banner.id} bannerContent={banner} />;
+        })}
+      </div>
     </div>
   );
 };
