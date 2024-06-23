612530bd6e637a97bc75a02ac0016cf754571de7
[vengadaganapathy1] <upskilltechnology24@gmail.com>
2024-06-23 12:27:25 +0530

    Updated code

--- utils/constants.js
+++ utils/constants.js
@@ -8,5 +8,8 @@ export const RESTAURANT_LIST_URL =
 export const RESTAURANT_DETAILS_URL =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.89960&lng=80.22090&catalog_qa=undefined&submitAction=ENTER&restaurantId=";
 
+export const MENU_DETAILS_IMAGE_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
+
 export const GIT_USER_DETAILS =
   "https://api.github.com/users/vengadaganapathy1";
