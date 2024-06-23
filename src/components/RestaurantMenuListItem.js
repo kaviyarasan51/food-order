f16f78bcecb00d6b6ade16cf830798df13650af7
[vengadaganapathy1] <upskilltechnology24@gmail.com>
2024-06-23 11:23:05 +0530

    Updated code

--- src/components/RestaurantMenuListItem.js
+++ src/components/RestaurantMenuListItem.js
@@ -1,13 +1,13 @@
 const RestaurantMenuListItem = (props) => {
   const { id, imageId, ratings, defaultPrice, name, price, description } =
-    props.cardItems.card.info;
+    props?.cardItems?.card?.info;
   return (
     <div className='mx-10 py-5 border-b-2'>
       <p className='font-semibold text-lg'>{name}</p>
       <p className='font-semibold text-lg'>
         ₹ {(price ? price : defaultPrice) / 100}
       </p>
-      <p>{ratings.aggregatedRating.ratings}</p>
+      <p>{ratings.aggregatedRating.rating}</p>
       <p>{description}</p>
     </div>
   );
--- src/components/RestaurantOverview.js
+++ src/components/RestaurantOverview.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import useRestaurantMenuList from "../custom-hooks/useRestaurantMenuList";
-import RestaurantMenuListItem from "./RestaurantMenuListItem";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantOverview = () => {
   let { id } = useParams();
@@ -23,69 +23,11 @@ const RestaurantOverview = () => {
       <h1 className='font-extrabold text-2xl my-5'>{restaurantName}</h1>
       {categories.map((category) => {
         return (
-          <div
-            className='shadow-lg shadow-gray-300 mb-5'
-            key={category.card.card.title}>
-            <div className='flex justify-between bg-gray-300 py-2 px-5'>
-              <p className='font-semibold text-lg'>
-                {category.card.card.title}
-                {category.card.card?.itemCards?.length
-                  ? " (" + category.card.card?.itemCards?.length + " )"
-                  : ""}
-              </p>
-              <p>⏬</p>
-            </div>
-            {category.card.card["@type"] ===
-            "type.googleapis.com/swiggy.presentation.food.v2.NestedItemCategory" ? (
-              <div>
-                {category.card.card.categories.map((subCategory) => {
-                  return <p key={subCategory.title}>{subCategory.title}</p>;
-                })}
-                <p>Sub category</p>
-              </div>
-            ) : (
-              category.card.card.itemCards.map((item) => {
-                return (
-                  <RestaurantMenuListItem
-                    key={item.card.info.id}
-                    cardItems={item}
-                  />
-                );
-              })
-            )}
-          </div>
+          <RestaurantCategory
+            key={category.card.card.title}
+            category={category}
+          />
         );
-        // return (
-        //   <div className='shadow-lg shadow-gray-300 my-5'>
-        //     <div className='flex bg-gray-300 px-3 py-2'>
-        //       <p className='w-11/12 font-semibold text-xl'>
-        //         {category.card.card.title}
-        //       </p>
-        //       <p className='w-1/12'>🔽</p>
-        //     </div>
-        //     <div className='px-3 py-2'>
-        //       {category.card.card["@type"] ===
-        //       "type.googleapis.com/swiggy.presentation.food.v2.NestedItemCategory" ? (
-        //         category.card.card.categories.map((subCategory) => {
-        //           return (
-        //             <div>
-        //               <p className='font-medium text-xl'>{subCategory.title}</p>
-        //               {subCategory.itemCards.map((items) => {
-        //                 return <RestaurantMenuListItem cardItems={items} />;
-        //               })}
-        //             </div>
-        //           );
-        //         })
-        //       ) : (
-        //         <div>
-        //           {category.card.card.itemCards.map((items) => {
-        //             return <RestaurantMenuListItem cardItems={items} />;
-        //           })}
-        //         </div>
-        //       )}
-        //     </div>
-        //   </div>
-        // );
       })}
     </div>
   ) : (
